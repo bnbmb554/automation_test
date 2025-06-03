5027a4a3a8a15553c2942ade0df315ae17017eae
vivek yadav <vivekyadav031997@gmail.com>
2025-06-03 13:58:56 +0530

    Added Comment

--- PythonProjectsample/conftest.py
+++ PythonProjectsample/conftest.py
@@ -7,6 +7,7 @@ def page():
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
+    #Comment Added by Vivek
     page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     yield page
     browser.close()
